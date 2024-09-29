a622dfcf02b1a42bc35f91be48d3ee6683605482
Jetom88 <shinhro3o@gmail.com>
2024-09-29 22:13:58 +0900

    feat: login page 퍼블리싱 완성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,93 @@
 import Image from "next/image";
 import Logo from "@svgs/logo.svg";
-import { caption } from "@styles/font.css";
-import { loginTextBox, loginHeading } from "@styles/login/login.css";
-import { flexSprinkles } from "./components/common/utils/flex";
+
+import {
+  loginTextBox,
+  loginHeading,
+  loginCaption,
+  loginBox,
+  loginWrapper,
+  loginLogo,
+  loginBoxContents,
+  loginBoxContainer,
+} from "@styles/login/login.css";
+import { flexSprinklesFc } from "./components/common/utils/flex";
+import { regular } from "./styles/font.css";
+
+type LoginBoxProps = {
+  image: string;
+  text: string;
+  bg: string;
+  textColor: string;
+  border?: boolean;
+};
+
+const LoginBox = ({ image, text, bg, textColor, border }: LoginBoxProps) => {
+  return (
+    <div
+      className={loginBox}
+      style={{
+        background: bg,
+        color: textColor,
+        border: border ? "1px solid #d2d5d6" : "none",
+      }}
+    >
+      <div className={loginBoxContents}>
+        <Image src={image} alt={text} width={24} height={24} />
+        <p className={regular}>{text}</p>
+      </div>
+    </div>
+  );
+};
 
 export default function Login() {
-  const logoClassName = flexSprinkles({
+  const logoClassName = flexSprinklesFc({
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
     gap: "24px",
   });
   return (
-    <main>
-      <section>
-        <article>
-          <div className={logoClassName}>
-            <Image src={Logo} alt="logo" />
-            <div className={loginTextBox}>
-              <h2 className={loginHeading}>Eungeori</h2>
-              <p className={caption}>건강한 습관을 위한 스마트 앱</p>
-            </div>
-          </div>
-        </article>
-      </section>
-    </main>
+    <section
+      className={`${loginWrapper} ${flexSprinklesFc({
+        flexDirection: "column",
+        justifyContent: "space-between",
+      })}`}
+    >
+      <article className={`${logoClassName} ${loginLogo}`}>
+        <Image src={Logo} alt="logo" />
+        <div className={loginTextBox}>
+          <h2 className={loginHeading}>Eungeori</h2>
+          <p className={loginCaption}>건강한 습관을 위한 스마트 앱</p>
+        </div>
+      </article>
+
+      <article
+        className={`${loginBoxContainer} ${flexSprinklesFc({
+          flexDirection: "column",
+          gap: "8px",
+        })}`}
+      >
+        <LoginBox
+          image="/svgs/kakao.svg"
+          text="카카오톡으로 계속하기"
+          bg="#FEE500"
+          textColor="#242729"
+        />
+        <LoginBox
+          image="/svgs/naver.svg"
+          text="네이버로 계속하기"
+          bg="#00C53A"
+          textColor="#ffffff"
+        />
+        <LoginBox
+          image="/svgs/google.svg"
+          text="Google로 계속하기"
+          bg="#ffffff"
+          textColor="#242729"
+          border
+        />
+      </article>
+    </section>
   );
 }
--- src/app/styles/login/login.css.ts
+++ src/app/styles/login/login.css.ts
@@ -1,5 +1,22 @@
 import { style } from "@vanilla-extract/css";
-import { semiBold, heading } from "../font.css";
+import { semiBold, heading, caption } from "../font.css";
+import { paddingSprinkles } from "../padding.css";
+import { vars } from "../vars.css";
+
+export const loginWrapper = style([
+  {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    minHeight: "100%",
+  },
+  paddingSprinkles({ paddingX: "s28" }),
+]);
+
+export const loginLogo = style({
+  minHeight: "calc(730px)",
+});
 
 export const loginTextBox = style({
   textAlign: "center",
@@ -8,7 +25,32 @@ export const loginTextBox = style({
 export const loginHeading = style([
   semiBold,
   heading,
+  paddingSprinkles({ paddingBottom: "s12" }),
+]);
+
+export const loginCaption = style([
+  caption,
   {
-    paddingBottom: "28px",
+    color: "#909090",
   },
+]);
+
+export const loginBoxContainer = style({
+  width: "100%",
+});
+
+export const loginBox = style({
+  height: "60px",
+  borderRadius: vars.space.s12,
+  cursor: "pointer",
+});
+
+export const loginBoxContents = style([
+  {
+    display: "grid",
+    gridTemplateColumns: "1fr 2.5fr",
+    alignItems: "center",
+    height: "100%",
+  },
+  paddingSprinkles({ paddingLeft: "s24" }),
 ]);
