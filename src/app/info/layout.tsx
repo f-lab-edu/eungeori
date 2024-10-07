efbddbd511c176ba1ffc951bbba5f01ce1ec3a1d
Jetom88 <shinhro3o@gmail.com>
2024-10-07 18:48:48 +0900

    fix: layout의 버튼을 각각 페이지로 이동

--- src/app/info/layout.tsx
+++ src/app/info/layout.tsx
@@ -4,9 +4,6 @@ import { useRouter } from "next/navigation";
 import { infoWrapper } from "../styles/info/common.css";
 import Image from "next/image";
 import { pointer } from "../styles/global.css";
-import { flexSprinklesFc } from "../components/common/utils/flex";
-import { colors } from "../styles/colors.css";
-import Button from "../components/common/Button";
 import { paddingSprinkles } from "../styles/padding.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -18,28 +15,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       </div>
 
       {children}
-
-      <div className={flexSprinklesFc({ gap: "16px", justifyContent: "center" })}>
-        <Button
-          text="취소"
-          height="59px"
-          borderRadius="10px"
-          onClick={() => {
-            router.push("/record");
-          }}
-        />
-        <Button
-          text="다음"
-          width="226px"
-          height="59px"
-          background={colors.primary}
-          color={colors.white}
-          borderRadius="10px"
-          onClick={() => {
-            router.push("/info/shape");
-          }}
-        />
-      </div>
     </section>
   );
 }
