e876c71758733bd272d21b1353ed058c46982f7a
Jetom88 <shinhro3o@gmail.com>
2024-10-18 23:57:46 +0900

    feat: 묽기 및 모양 클릭 가능하도록 로직 추가

--- src/app/info/shape/page.tsx
+++ src/app/info/shape/page.tsx
@@ -4,11 +4,12 @@ import Button from "@/app/components/common/Button";
 import { flexSprinklesFc } from "@/app/components/common/utils/flex";
 import { colors, gray300, primary, white } from "@/app/styles/colors.css";
 import { heading2, semiBold } from "@/app/styles/font.css";
-import { infoBox } from "@/app/styles/info/common.css";
+import { pointer } from "@/app/styles/global.css";
 import { shapeContentBox, shapeContentBoxWrapper, shapeContentBoxText } from "@/app/styles/info/shape.css";
 import { paddingSprinkles } from "@/app/styles/padding.css";
 import Image, { ImageProps } from "next/image";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 type ContentBoxProps = {
   src: string;
@@ -16,12 +17,13 @@ type ContentBoxProps = {
   width: ImageProps["width"];
   height: ImageProps["height"];
   active?: boolean;
+  onClick: (label: string) => void;
 };
 
-const ContentBox = ({ src, text, width, height, active }: ContentBoxProps) => {
+const ContentBox = ({ src, text, width, height, active, onClick }: ContentBoxProps) => {
   return (
-    <div className={shapeContentBoxWrapper}>
-      <div className={shapeContentBox}>
+    <div className={shapeContentBoxWrapper} onClick={() => onClick(text)}>
+      <div className={`${shapeContentBox} ${pointer}`}>
         <Image src={src} alt={text} width={width} height={height} />
       </div>
       <p className={`${shapeContentBoxText} ${active ? primary : gray300}`}>{text}</p>
@@ -31,9 +33,51 @@ const ContentBox = ({ src, text, width, height, active }: ContentBoxProps) => {
 
 const page = () => {
   const router = useRouter();
+  const [stoolAttributes, setStoolAttributes] = useState({
+    consistency: "thin",
+    shapeType: "poop-1",
+  });
+
+  const onConsistencyChange = (consistency: string) => {
+    setStoolAttributes((prev) => ({ ...prev, consistency: consistency }));
+  };
+
+  const onShapeChange = (shape: string) => {
+    setStoolAttributes((prev) => ({ ...prev, shapeType: shape }));
+  };
+
+  const getConsistencyImageSrc = (label: string) => {
+    return stoolAttributes.consistency === label
+      ? `/svgs/poop/thin/active-${label}.svg`
+      : `/svgs/poop/thin/${label}.svg`;
+  };
+
+  const getShapeTypeImageSrc = (level: number) => {
+    return stoolAttributes.shapeType === `poop-${level}`
+      ? `/svgs/poop/active-poop-${level}.svg`
+      : `/svgs/poop/poop-${level}.svg`;
+  };
+
+  const consistencyOption = [
+    { label: "묽음", consistency: "thin" },
+    { label: "정상", consistency: "default" },
+    { label: "딱딱", consistency: "hard" },
+  ];
+
+  const shapeTypeOption =
+    //
+    [
+      { label: "단단한 덩어리", width: 71, height: 31, shape: "poop-1" },
+      { label: "단단한 소시지", width: 63, height: 30, shape: "poop-2" },
+      { label: "표면에 균열 있는 소시지", width: 67, height: 25, shape: "poop-3" },
+      { label: "매끈한 소시지", width: 70, height: 14, shape: "poop-4" },
+      { label: "부드러운 덩어리", width: 59, height: 31, shape: "poop-5" },
+      { label: "흐물흐물한 덩어리", width: 69, height: 33, shape: "poop-6" },
+      { label: "완전한 액체", width: 68, height: 90, shape: "poop-7" },
+    ];
 
   return (
-    <article className={infoBox}>
+    <article>
       <div className={`${flexSprinklesFc({ flexDirection: "column", gap: "16px" })} `}>
         <h3 className={`${semiBold} ${heading2} ${paddingSprinkles({ paddingBottom: "s60" })}`}>
           묽기 및 모양을
@@ -46,41 +90,39 @@ const page = () => {
           <p className={`${semiBold} ${paddingSprinkles({ paddingBottom: "s24" })}`}>묽음 정도</p>
 
           <div className={`${flexSprinklesFc({ gap: "16px" })}`}>
-            <ContentBox src="/svgs/poop/thin/thin.svg" width={42.5} height={42.5} text="묽음" />
-            <ContentBox src="/svgs/poop/thin/default.svg" width={42.5} height={42.5} text="정상" />
-            <ContentBox src="/svgs/poop/thin/crackle.svg" width={42.5} height={42.5} text="딱딱" />
+            {consistencyOption.map((image) => {
+              return (
+                <ContentBox
+                  key={image.label}
+                  src={getConsistencyImageSrc(image.consistency)}
+                  text={image.label}
+                  width={42.5}
+                  height={42.5}
+                  onClick={() => onConsistencyChange(image.consistency)}
+                  active={stoolAttributes.consistency === image.consistency}
+                />
+              );
+            })}
           </div>
         </div>
 
         <div className={paddingSprinkles({ paddingBottom: "s68" })}>
-          <p className={`${semiBold} ${paddingSprinkles({ paddingBottom: "s24" })}`}>묽음 정도</p>
+          <p className={`${semiBold} ${paddingSprinkles({ paddingBottom: "s24" })}`}>변의 모양</p>
 
           <div className={`${flexSprinklesFc({ gap: "16px", flexWrap: "wrap" })}`}>
-            <ContentBox src="/svgs/poop/poop-1.svg" width={71} height={31} text="단단한 덩어리" />
-            <ContentBox src="/svgs/poop/poop-2.svg" width={63} height={30} text="단단한 소시지" />
-            <ContentBox
-              src="/svgs/poop/poop-3.svg"
-              width={67}
-              height={25}
-              text="표면에 균열 있는
-소시지"
-            />
-            <ContentBox src="/svgs/poop/poop-4.svg" width={70} height={14} text="매끈한 소시지" />
-            <ContentBox
-              src="/svgs/poop/poop-5.svg"
-              width={59}
-              height={31}
-              text="부드러운 
-덩어리"
-            />
-            <ContentBox
-              src="/svgs/poop/poop-6.svg"
-              width={69}
-              height={33}
-              text="흐물흐물한
-덩어리"
-            />
-            <ContentBox src="/svgs/poop/poop-7.svg" width={68} height={94} text="완전한 액체" />
+            {shapeTypeOption.map((image, idx) => {
+              return (
+                <ContentBox
+                  key={image.label}
+                  src={getShapeTypeImageSrc(idx + 1)}
+                  text={image.label}
+                  width={image.width}
+                  height={image.height}
+                  onClick={() => onShapeChange(image.shape)}
+                  active={stoolAttributes.shapeType === image.shape}
+                />
+              );
+            })}
           </div>
         </div>
       </div>
