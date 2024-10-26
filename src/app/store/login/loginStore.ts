60cd2151b68075f633aa9f65f8b7631291aabc0c
Jetom88 <shinhro3o@gmail.com>
2024-10-26 16:52:17 +0900

    review: immer 라이브러리를 활용해 불변성 관리 및 상태 복사 작업 줄임

--- src/app/store/login/loginStore.ts
+++ src/app/store/login/loginStore.ts
@@ -1,20 +1,27 @@
 import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
 
 type LoginStore = {
-  loginPopup: boolean;
-  setLoginPopup: (state: boolean) => void;
+  isLoginPopup: boolean;
+  setIsLoginPopup: (state: boolean) => void;
 
   loginMessage: null | string;
   setLoginMessage: (state: string) => void;
 };
 
-export const useLoginStore = create<LoginStore>((set) => ({
-  loginPopup: false,
-  setLoginPopup: (state) => {
-    set(() => ({ loginPopup: state }));
-  },
-  loginMessage: null,
-  setLoginMessage: (state) => {
-    set(() => ({ loginMessage: state }));
-  },
-}));
+export const useLoginStore = create<LoginStore>()(
+  immer((set) => ({
+    isLoginPopup: false,
+    setIsLoginPopup: (state) => {
+      set((draft) => {
+        draft.isLoginPopup = state;
+      });
+    },
+    loginMessage: null,
+    setLoginMessage: (state) => {
+      set((draft) => {
+        draft.loginMessage = state;
+      });
+    },
+  }))
+);
