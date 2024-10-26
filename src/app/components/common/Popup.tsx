60783b42b24ada0c91a0b04ff0bbfd600f9992fb
Jetom88 <shinhro3o@gmail.com>
2024-10-26 16:51:22 +0900

    review: zod를 사용해서 text props를 nullable 타입으로 수정

--- src/app/components/common/Popup.tsx
+++ src/app/components/common/Popup.tsx
@@ -1,8 +1,11 @@
 import { popupContents, popupWrapper } from "@/app/styles/common/popup.css";
 import { paragraph2, semiBold } from "@/app/styles/font.css";
 import { flexSprinklesFc } from "./utils/flex";
+import { z } from "zod";
 
-const Popup = ({ text, children }: { text: null | string; children: React.ReactNode }) => {
+const textSchema = z.string().nullable();
+
+const Popup = ({ text, children }: { text: z.infer<typeof textSchema>; children: React.ReactNode }) => {
   return (
     <section className={popupWrapper}>
       <article className={`${popupContents} ${flexSprinklesFc({ flexDirection: "column", gap: "24px" })}`}>
