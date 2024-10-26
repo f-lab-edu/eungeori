2b1fa79044098869a1fc6d774632d70114526c87
Jetom88 <shinhro3o@gmail.com>
2024-10-26 19:59:05 +0900

    feat: login, signup의 input을 common으로 추가

---
+++ src/app/auth/common/inputUI.tsx
@@ -0,0 +1,30 @@
+import { inputStyle } from "@/app/components/common/input.css";
+import { pink80 } from "@/app/styles/colors.css";
+import { caption, regular } from "@/app/styles/font.css";
+import { paddingSprinkles } from "@/app/styles/padding.css";
+import { HTMLInputTypeAttribute } from "react";
+import { UseFormRegister } from "react-hook-form";
+
+type InputUiProps = {
+  text: string;
+  type?: HTMLInputTypeAttribute;
+  maxLegnth: number;
+  register: ReturnType<UseFormRegister<any>>;
+  errorMessage: string | undefined;
+};
+
+const FormInputUI = ({ text, type, maxLegnth, register, errorMessage }: InputUiProps) => {
+  return (
+    <div>
+      <p className={`${caption} ${regular} ${paddingSprinkles({ paddingBottom: "s4" })}`}>
+        {text}
+        <span className={`${pink80}`}>*</span>
+      </p>
+      <input placeholder={text} className={inputStyle} {...register} maxLength={maxLegnth} type={type} />
+
+      <p className={`${paddingSprinkles({ paddingTop: "s4" })} ${caption} ${pink80}`}>{errorMessage}</p>
+    </div>
+  );
+};
+
+export default FormInputUI;
