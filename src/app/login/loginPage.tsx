686f08f4a16293cbbaf8aba578605f5294ad62c5
Jetom88 <shinhro3o@gmail.com>
2024-10-26 16:52:41 +0900

    review: 불필요한 주석 제거

--- src/app/login/loginPage.tsx
+++ src/app/login/loginPage.tsx
@@ -7,33 +7,6 @@ import { loginWrapper, loginLogo, loginTextBox, loginHeading, loginCaption } fro
 import LoginForm from "./components/loginForm";
 import LoginPopup from "./components/loginPopup";
 
-// type LoginBoxProps = {
-//   image: string;
-//   text: string;
-//   bg: string;
-//   textColor: string;
-//   border?: boolean;
-//   onClick: MouseEventHandler<HTMLDivElement>;
-// };
-
-// const LoginBox = ({ image, text, bg, textColor, border, onClick }: LoginBoxProps) => {
-//   return (
-//     <div
-//       className={loginBox}
-//       style={{
-//         background: bg,
-//         color: textColor,
-//         border: border ? "1px solid #d2d5d6" : "none",
-//       }}
-//     >
-//       <div className={loginBoxContents} onClick={onClick}>
-//         <Image src={image} alt={text} width={24} height={24} />
-//         <p className={regular}>{text}</p>
-//       </div>
-//     </div>
-//   );
-// };
-
 export default function LoginPage() {
   const logoClassName = flexSprinklesFc({
     flexDirection: "column",
@@ -59,29 +32,6 @@ export default function LoginPage() {
 
           <LoginForm />
         </article>
-
-        {/* <LoginBox
-          image="/svgs/kakao.svg"
-          text="카카오톡으로 계속하기"
-          bg="#FEE500"
-          textColor="#242729"
-          onClick={() => router.push("/record")}
-        />
-        <LoginBox
-          image="/svgs/naver.svg"
-          text="네이버로 계속하기"
-          bg="#00C53A"
-          textColor="#ffffff"
-          onClick={() => router.push("/record")}
-        />
-        <LoginBox
-          image="/svgs/google.svg"
-          text="Google로 계속하기"
-          bg="#ffffff"
-          textColor="#242729"
-          onClick={() => router.push("/record")}
-          border
-        /> */}
       </section>
     </>
   );
