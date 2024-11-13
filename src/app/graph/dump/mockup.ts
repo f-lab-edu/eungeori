fab2d9df9e3134e51e61bfd97bcfac8e332c86de
Jetom88 <shinhro3o@gmail.com>
2024-11-13 18:37:16 +0900

    review: 목업 데이터 분리

---
+++ src/app/graph/dump/mockup.ts
@@ -0,0 +1,219 @@
+type BowelDateCount = Record<string, number>;
+
+export const bowelInfoDate30Days = [
+  {
+    bowelTime: {
+      hour: 5,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "default", shapeType: "poop-1" },
+    date: "24.09.01",
+  },
+  {
+    bowelTime: {
+      hour: 5,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "default", shapeType: "poop-1" },
+    date: "24.09.02",
+  },
+  {
+    bowelTime: {
+      hour: 5,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "crackle", shapeType: "poop-1" },
+    date: "24.09.03",
+  },
+  {
+    bowelTime: {
+      hour: 5,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "crackle", shapeType: "poop-1" },
+    date: "24.09.04",
+  },
+  {
+    bowelTime: {
+      hour: 5,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "crackle", shapeType: "poop-1" },
+    date: "24.09.05",
+  },
+  {
+    bowelTime: {
+      hour: 5,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "thin", shapeType: "poop-1" },
+    date: "24.09.06",
+  },
+  {
+    bowelTime: {
+      hour: 5,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "thin", shapeType: "poop-1" },
+    date: "24.09.07",
+  },
+  {
+    bowelTime: {
+      hour: 5,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "crackle", shapeType: "poop-1" },
+    date: "24.09.08",
+  },
+  {
+    bowelTime: {
+      hour: 5,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "crackle", shapeType: "poop-1" },
+    date: "24.09.09",
+  },
+  {
+    bowelTime: {
+      hour: 19,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "thin", shapeType: "poop-1" },
+    date: "24.09.09",
+  },
+  {
+    bowelTime: {
+      hour: 13,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "default", shapeType: "poop-1" },
+    date: "24.09.10",
+  },
+  {
+    bowelTime: {
+      hour: 16,
+      minute: 10,
+    },
+    stoolAttributes: { consistency: "crackle", shapeType: "poop-4" },
+    date: "24.09.10",
+  },
+  {
+    bowelTime: {
+      hour: 20,
+      minute: 10,
+    },
+    stoolAttributes: { consistency: "crackle", shapeType: "poop-2" },
+    date: "24.09.10",
+  },
+  {
+    bowelTime: {
+      hour: 16,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "default", shapeType: "poop-1" },
+    date: "24.09.13",
+  },
+  {
+    bowelTime: {
+      hour: 3,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "default", shapeType: "poop-1" },
+    date: "24.09.20",
+  },
+  {
+    bowelTime: {
+      hour: 3,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "default", shapeType: "poop-1" },
+    date: "24.09.23",
+  },
+  {
+    bowelTime: {
+      hour: 3,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "default", shapeType: "poop-1" },
+    date: "24.09.24",
+  },
+  {
+    bowelTime: {
+      hour: 3,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "default", shapeType: "poop-1" },
+    date: "24.09.25",
+  },
+];
+
+export const bowelInfoDate7Days = [
+  {
+    bowelTime: {
+      hour: 5,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "thin", shapeType: "poop-1" },
+    date: "24.09.09",
+  },
+  {
+    bowelTime: {
+      hour: 19,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "thin", shapeType: "poop-1" },
+    date: "24.09.09",
+  },
+  {
+    bowelTime: {
+      hour: 13,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "default", shapeType: "poop-1" },
+    date: "24.09.10",
+  },
+  {
+    bowelTime: {
+      hour: 16,
+      minute: 10,
+    },
+    stoolAttributes: { consistency: "crackle", shapeType: "poop-4" },
+    date: "24.09.10",
+  },
+  {
+    bowelTime: {
+      hour: 20,
+      minute: 10,
+    },
+    stoolAttributes: { consistency: "crackle", shapeType: "poop-2" },
+    date: "24.09.10",
+  },
+  {
+    bowelTime: {
+      hour: 16,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "default", shapeType: "poop-1" },
+    date: "24.09.13",
+  },
+  {
+    bowelTime: {
+      hour: 3,
+      minute: 20,
+    },
+    stoolAttributes: { consistency: "default", shapeType: "poop-1" },
+    date: "24.09.20",
+  },
+];
+
+export const bowelDateCount = (data) => {
+  return data.reduce((acc: BowelDateCount, cur) => {
+    const date = cur.date;
+    acc[date] = (acc[date] || 0) + 1;
+    return acc;
+  }, {});
+};
+
+export const consistency = (data) => {
+  return data.map((x) => x.stoolAttributes.consistency);
+};
