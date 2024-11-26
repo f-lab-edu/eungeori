bcaa05e6014b79a8f2cd3cbfb099c3acd2a0e097
Jetom88 <shinhro3o@gmail.com>
2024-11-26 23:34:37 +0900

    fix: email 유효성 검사 및 nickname 유효성 검사 로직 추가

--- src/app/auth/signup/hook/useSignup.ts
+++ src/app/auth/signup/hook/useSignup.ts
@@ -3,35 +3,58 @@ import { usePopupStore } from "@/app/store/popup/PopupStore";
 import { signupSchema } from "@/app/types/signupSchema";
 import { z } from "zod";
 
+const checkEmailExists = async (email: string) => {
+  const { data, error } = await supabase.rpc("check_email_exists", { email_input: email });
+
+  if (error) {
+    console.error("RPC 호출 오류:", error.message);
+    return false;
+  }
+
+  return data; // boolean
+};
+
+const checkNicknameExists = async (nickname: string) => {
+  const { data, error } = await supabase.rpc("check_nickname_exists", { nickname });
+
+  if (error) {
+    console.error("RPC 호출 오류:", error.message);
+    return false;
+  }
+
+  return data; // boolean
+};
+
 export const useSignup = () => {
   const setIsPopupState = usePopupStore((state) => state.setIsPopup);
   const setMessageState = usePopupStore((state) => state.setMessage);
 
   const onSignupSubmit = async (data: z.infer<typeof signupSchema>) => {
     try {
-      const { data: existingUser } = await supabase
-        .from("signup")
-        .select("username, nickname")
-        .or(`username.eq.${data.id},nickname.eq.${data.nickname}`)
-        .single();
+      const isNicknameExists = await checkNicknameExists(data.nickname);
+      const isEmailExists = await checkEmailExists(data.email);
 
-      const { error } = await supabase.from("signup").insert({
-        username: data.id,
-        password: data.password,
-        nickname: data.nickname,
-      });
-
-      if (existingUser?.username) {
+      if (isEmailExists) {
         setIsPopupState(true);
-        setMessageState("이미 존재하는 회원입니다.");
+        setMessageState("이미 가입된 이메일입니다.");
         return;
       }
 
-      if (existingUser?.nickname) {
+      if (isNicknameExists) {
         setIsPopupState(true);
         setMessageState("이미 존재하는 닉네임입니다.");
         return;
       }
+
+      const { data: userData, error } = await supabase.auth.signUp({
+        email: data.email,
+        password: data.password,
+        options: {
+          data: {
+            nickname: data.nickname,
+          },
+        },
+      });
 
       if (error) {
         setIsPopupState(true);
