422757d3a8cf55102a3ffed96bfe2222c1928ca8
Jetom88 <shinhro3o@gmail.com>
2024-11-28 22:18:28 +0900

    feat: image upload 로직 구현중

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
-const {
-    createVanillaExtractPlugin
-  } = require('@vanilla-extract/next-plugin');
-  const withVanillaExtract = createVanillaExtractPlugin();
-  
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {};
-  
-  module.exports = withVanillaExtract(nextConfig);
+const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+const withVanillaExtract = createVanillaExtractPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "qzohcyvjvdvxzbblmbtx.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
+};
+
+module.exports = withVanillaExtract(nextConfig);
--- src/app/my/page.tsx
+++ src/app/my/page.tsx
@@ -8,35 +8,156 @@ import { paddingSprinkles } from "../styles/padding.css";
 import { gray300 } from "../styles/colors.css";
 import { buttonOutLine, pointer } from "../styles/global.css";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { LocalStorage } from "../types/localStorageSchema";
+import { useEffect, useRef, useState } from "react";
 import { inputStyle } from "../components/common/input.css";
+import { supabase, userProfile } from "../lib/supabaseClient";
+import { usePopupStore } from "../store/popup/PopupStore";
 
 const Page = () => {
   const router = useRouter();
+  const [userInfo, setUserInfo] = useState({
+    nickname: "",
+    id: "",
+  });
+  const [imageUrl, setImageUrl] = useState<string>("/image/profile.png");
   const [goal, setGoal] = useState<string>("");
 
-  const goalStorage = new LocalStorage("goal");
-  const signinStorage = new LocalStorage("signin");
+  const setIsPopupState = usePopupStore((state) => state.setIsPopup);
+  const setMessageState = usePopupStore((state) => state.setMessage);
 
-  const onClick = () => {
-    signinStorage.remove();
-
-    router.push("/");
-  };
-
-  const onGoalSave = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      goalStorage.set(goal);
-    }
-  };
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
 
   useEffect(() => {
-    const saveGoal = goalStorage.get();
-    if (saveGoal) {
-      setGoal(saveGoal);
-    }
+    const getUserInfoData = async () => {
+      const user = await supabase.auth.getUser();
+      const nickname = user.data.user?.user_metadata.nickname;
+      const id = user.data.user?.id;
+
+      setUserInfo({
+        nickname: nickname || "",
+        id: id || "",
+      });
+    };
+
+    getUserInfoData();
   }, []);
+
+  useEffect(() => {
+    const getUserProfile = async () => {
+      try {
+        const { data, error } = await supabase.auth.getUser();
+        const id = userInfo.id;
+
+        if (!data.user || error) {
+          setIsPopupState(true);
+          setMessageState("사용자 정보를 불러오는데 실패했습니다.");
+        }
+
+        // 로컬호스트로 바라보는것도 문제
+        // 파일 이름을 넣기.. id 말고
+        const filePath = `user_profile_image/${id}`;
+
+        const { data: urlData } = supabase.storage.from(`${userProfile}`).getPublicUrl(filePath);
+        if (!urlData.publicUrl) {
+          setIsPopupState(true);
+          setMessageState("프로필 이미지를 불러오는데 실패했습니다.");
+        }
+
+        setImageUrl(urlData.publicUrl);
+      } catch (e) {
+        setIsPopupState(true);
+        setMessageState("알 수 없는 오류가 발생했습니다.");
+      }
+    };
+
+    getUserProfile();
+  }, []);
+
+  const onClick = async () => {
+    try {
+      const { error } = await supabase.auth.signOut();
+      if (error) {
+        setIsPopupState(true);
+        setMessageState("로그아웃 실패. 다시 시도해주세요.");
+        return;
+      }
+      router.push("/");
+      return;
+    } catch (e) {
+      setIsPopupState(true);
+      setMessageState("알 수 없는 오류가 발생했습니다.");
+    }
+  };
+
+  const onGoalSave = async (goal: string) => {
+    try {
+      if (!userInfo.id || !goal.trim()) {
+        setIsPopupState(true);
+        setMessageState("유효하지 않은 입력값입니다.");
+        return;
+      }
+
+      const { data, error } = await supabase.from("user_profile").insert([{ goal, id: userInfo.id }]);
+
+      console.log(data, "test");
+
+      if (error) {
+        console.log(error, "error");
+        setIsPopupState(true);
+        setMessageState("알 수 없는 오류가 발생했습니다.");
+        return;
+      }
+
+      setIsPopupState(true);
+      setMessageState("저장 되었습니다.");
+      setGoal("");
+    } catch (e) {
+      setIsPopupState(true);
+      setMessageState("알 수 없는 오류가 발생했습니다.");
+      return;
+    }
+  };
+
+  const handleKeyDown = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && goal.trim()) {
+      await onGoalSave(goal);
+    }
+  };
+
+  const handleImageClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      await uploadImage(file);
+    }
+  };
+
+  const uploadImage = async (file: File) => {
+    try {
+      const filePath = `user_profile_image/${file.name}`;
+      const { data, error } = await supabase.storage
+        .from(`${userProfile}`)
+        .upload(filePath, file, { upsert: true });
+
+      if (error) {
+        setIsPopupState(true);
+        setMessageState("업로드에 실패하였습니다.");
+        return;
+      }
+
+      setIsPopupState(true);
+      setMessageState("변경되었습니다.");
+    } catch (e) {
+      setIsPopupState(true);
+      setMessageState("알 수 없는 오류가 발생했습니다.");
+    }
+  };
+
   return (
     <>
       <div>
@@ -48,8 +169,19 @@ const Page = () => {
             gap: "8px",
           })}`}
         >
-          <p className={`${semiBold} ${paragraph}`}>제톰 님, 안녕하세요</p>
-          <Image src="/image/profile.png" width={110} height={110} alt="profile" priority />
+          <p className={`${semiBold} ${paragraph}`}>{userInfo.nickname}님, 안녕하세요</p>
+          <form>
+            <input type="file" style={{ display: "none" }} ref={fileInputRef} onChange={handleFileChange} />
+          </form>
+          <Image
+            className={pointer}
+            src={imageUrl}
+            width={110}
+            height={110}
+            alt="profile"
+            priority
+            onClick={handleImageClick}
+          />
         </div>
 
         <div className={myTargetContainer}>
@@ -63,9 +195,7 @@ const Page = () => {
             onChange={(e) => {
               setGoal(e.target.value);
             }}
-            onKeyDown={(e) => {
-              onGoalSave(e);
-            }}
+            onKeyDown={handleKeyDown}
           />
         </div>
       </div>
