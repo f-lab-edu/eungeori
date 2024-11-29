7e369cb261279ecf04e0c1436f819028f284c3c6
Jetom88 <shinhro3o@gmail.com>
2024-11-29 23:10:42 +0900

    feat: 서비스 롤 키 추가

--- src/app/lib/supabaseClient.ts
+++ src/app/lib/supabaseClient.ts
@@ -3,5 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 export const userProfile = process.env.NEXT_PUBLIC_STORAGE_BUCKET as string;
+const serviceRolKey = process.env.NEXT_PUBLIC_SERVICE_ROL_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+export const admin = createClient(supabaseUrl, serviceRolKey);
