ad48151d64c7e03288e47ff2cfbab73eef451af9
Jetom88 <shinhro3o@gmail.com>
2024-12-31 18:51:27 +0900

    fix: span 태그 대신 button태그로 수정

--- src/app/components/common/Memo.tsx
+++ src/app/components/common/Memo.tsx
@@ -1,9 +1,9 @@
 import { caption2, subFontStyle } from '@/app/styles/font.css';
 import Image from 'next/image';
 import { gray300 } from '@/app/styles/colors.css';
-import { pointer } from '@/app/styles/global.css';
 import { flexSprinklesFc } from './utils/flex';
 import { memoBox } from './memo.css';
+import useInfoStore from '@/app/store/info/infoStore';
 
 type MemoProps = {
   onEditClick?: () => void;
@@ -12,7 +12,7 @@ type MemoProps = {
   text?: string;
   height?: string;
   edit?: boolean;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  isReadOnly?: boolean;
 };
 
 const Memo = ({
@@ -22,8 +22,9 @@ const Memo = ({
   text,
   height = '200px',
   edit = false,
-  onChange,
+  isReadOnly = false,
 }: MemoProps) => {
+  const setRecordNote = useInfoStore((state) => state.setRecordNote);
   return (
     <div className={memoBox} style={{ height }}>
       <Image src="/svgs/comment.svg" alt="icon" width={20} height={19} />
@@ -32,20 +33,15 @@ const Memo = ({
         <input
           value={text ? text : ''}
           className={subFontStyle}
-          onChange={onChange}
-          readOnly={!onChange}
+          onChange={(e) => !isReadOnly && setRecordNote(e.target.value)}
+          readOnly={isReadOnly}
         />
       </div>
 
       {edit && (
         <p className={`${caption2} ${gray300}`}>
-          <span className={pointer} onClick={onEditClick}>
-            수정
-          </span>{' '}
-          <span>|</span>{' '}
-          <span className={pointer} onClick={onDeleteClick}>
-            삭제
-          </span>
+          <button onClick={onEditClick}>수정</button> <span>|</span>{' '}
+          <button onClick={onDeleteClick}>삭제</button>
         </p>
       )}
     </div>
