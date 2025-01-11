0f6aa609b5df43edeecea7afd6b2add5affa8ac4
Jetom88 <shinhro3o@gmail.com>
2025-01-11 17:29:11 +0900

    fix: 테스트 코드 작성 로직 수정

--- src/app/__tests__/loginForm.test.tsx
+++ src/app/__tests__/loginForm.test.tsx
@@ -1,48 +1,50 @@
 import '@testing-library/jest-dom';
-import { screen, render } from '@testing-library/react';
-import { UseFormRegisterReturn } from 'react-hook-form';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FormInputUI from '../auth/common/inputUI';
 
-interface InputUiProps {
-  text: string;
-  type: string;
-  maxLength: number;
-  register: UseFormRegisterReturn;
-  errorMessage?: string;
-}
+jest.mock('../auth/common/padding.css', () => ({
+  paddingSprinkles: () => 'mocked-padding-class',
+}));
 
-const FormInputUI = ({ text, type, maxLength, register, errorMessage }: InputUiProps) => {
-  return (
-    <div>
-      <p>
-        {text}
-        <span>*</span>
-      </p>
-      <input placeholder={text} {...register} maxLength={maxLength} type={type} />
-
-      <p>{errorMessage}</p>
-    </div>
-  );
-};
-
-describe('loginForm', () => {
-  const mockRegister: UseFormRegisterReturn = {
-    onChange: jest.fn(),
-    onBlur: jest.fn(),
-    name: 'email',
-    ref: jest.fn(),
-  };
-
-  const mockProps: InputUiProps = {
-    text: 'email',
+describe(FormInputUI, () => {
+  const formInputUIProps = {
+    text: '이메일',
     type: 'email',
     maxLength: 50,
-    register: mockRegister,
-    errorMessage: '이메일을 입력해주세요',
+    register: {
+      onChange: jest.fn(),
+      onBlur: jest.fn(),
+      name: 'email',
+      ref: jest.fn(),
+    },
+    errorMessage: '이메일을 입력하세요',
   };
 
-  it('이메일 입력 필드 렌더링', () => {
-    render(<FormInputUI {...mockProps} />);
-    const input = screen.getByPlaceholderText('email');
-    expect(input).toBeInTheDocument();
+  const getInput = () => screen.getByPlaceholderText(formInputUIProps.text);
+
+  beforeEach(() => {
+    render(<FormInputUI {...formInputUIProps} />);
+  });
+
+  it('렌더링이 정상적으로 작동된다', () => {
+    expect(screen.getByText('*')).toBeInTheDocument();
+    expect(screen.getByText(formInputUIProps.text)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(formInputUIProps.text)).toBeInTheDocument();
+  });
+
+  it('에러 메시지가 표시된다', () => {
+    expect(screen.getByText(formInputUIProps.errorMessage)).toBeInTheDocument();
+  });
+
+  it('입력 제한이 된다', () => {
+    expect(getInput()).toHaveAttribute('maxLength', String(formInputUIProps.maxLength));
+  });
+
+  it('사용자 입력이 정상적으로 동작한다', async () => {
+    const user = userEvent.setup();
+
+    await user.type(getInput(), 'jetom.shin@gmail.com');
+    expect(getInput()).toHaveValue('jetom.shin@gmail.com');
   });
 });
